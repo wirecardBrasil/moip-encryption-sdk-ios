0662a40379b8f94b2ca483ed9e0d88860ce80543
Fernando Nazario Sousa <fernandosousa@MacBook-Pro-de-Fernando.local>
2014-03-05 12:34:12 -0300

    Initial Commit

---
+++ MoipSDK/MoipSDK.h
@@ -0,0 +1,13 @@
+//
+//  MoipSDK.h
+//  MoipSDK
+//
+//  Created by Fernando Nazario Sousa on 05/03/14.
+//  Copyright (c) 2014 Moip Pagamentos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoipSDK : NSObject
+
+@end
---
+++ MoipSDK/MoipSDK.m
@@ -0,0 +1,13 @@
+//
+//  MoipSDK.m
+//  MoipSDK
+//
+//  Created by Fernando Nazario Sousa on 05/03/14.
+//  Copyright (c) 2014 Moip Pagamentos. All rights reserved.
+//
+
+#import "MoipSDK.h"
+
+@implementation MoipSDK
+
+@end
